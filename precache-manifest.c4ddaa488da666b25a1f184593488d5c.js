4fa4c9a8971c499d29d38e07111e0a08a27e034c
SamirRodrigues <emaildesamir@gmail.com>
2021-09-27 03:49:26 -0300

    Updates

---
+++ precache-manifest.c4ddaa488da666b25a1f184593488d5c.js
@@ -0,0 +1,26 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "40858a3b4162bcd04aff4f9a933e924a",
+    "url": "/BirthdayReminder/index.html"
+  },
+  {
+    "revision": "165b5352d28032232237",
+    "url": "/BirthdayReminder/static/css/main.10d6a708.chunk.css"
+  },
+  {
+    "revision": "6e2d7a9fbc772ec3686f",
+    "url": "/BirthdayReminder/static/js/2.1f771bf1.chunk.js"
+  },
+  {
+    "revision": "e88a3e95b5364d46e95b35ae8c0dc27d",
+    "url": "/BirthdayReminder/static/js/2.1f771bf1.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "165b5352d28032232237",
+    "url": "/BirthdayReminder/static/js/main.277d117f.chunk.js"
+  },
+  {
+    "revision": "050cd34c536148fe099c",
+    "url": "/BirthdayReminder/static/js/runtime-main.8147a367.js"
+  }
+]);
